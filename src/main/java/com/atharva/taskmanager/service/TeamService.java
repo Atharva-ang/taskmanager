f7eb85033ef5b6c0f91136d0c8302a331a4171aa
Atharva Gandhe <atharvanarayan0@gmail.com>
2026-03-01 23:05:49 +0530

    Update TeamService.java

--- src/main/java/com/atharva/taskmanager/service/TeamService.java
+++ src/main/java/com/atharva/taskmanager/service/TeamService.java
@@ -16,7 +16,7 @@ public class TeamService {
     private final TeamRepository teamRepository;
     private final UserRepository userRepository;
 
-    // 1. Create a new team AND add the creator to it securely
+    // 1. Create a new team from a new branch
     public Team createTeam(Team team, String userEmail) {
         User creator = userRepository.findByEmail(userEmail)
                 .orElseThrow(() -> new RuntimeException("User not found"));
